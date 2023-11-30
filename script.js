e795e169d2f48e3a5c6337aaeb2d011e5b1573ac
Dulce Paiva <151636786+paivadul@users.noreply.github.com>
2023-11-30 10:20:59 -0300

    Wordle PPY - Versión Final 1
    ¡Completa y funciona!

--- script.js
+++ script.js
@@ -6,8 +6,10 @@
 recorrer adivinar letra por letra
 */
 
-let palabra = 'APPLE';
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
+let palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
 let intento = 6;
+
 
 const BOTON = document.getElementById('guess-button') //boton intentar
 
@@ -17,12 +19,13 @@ BOTON.addEventListener('click', intentar)
 
 function intentar () {
     const GRID = document.getElementById('grid');
-    const ROW = document.createElement('row');
+    const ROW = document.createElement('div');
     ROW.className = 'row';
 
     const INPUT2 = document.getElementById("guess-input").value.toUpperCase();
     if (INPUT2 === palabra) {
-        console.log('Ganaste!')
+        terminar("<h1>GANASTE!</h1>")
+        console.log('GANASTE!')
         return;
     }
 
@@ -31,22 +34,34 @@ function intentar () {
         SPAN.className = 'letter'
         if (palabra[i] === INPUT2[i]) {
             console.log(INPUT2[i], 'verde');
-            SPAN.style.backgroundColor = 'green';
+            SPAN.style.backgroundColor = '#79b851';
+
             SPAN.innerHTML = INPUT2[i]
         } else if (palabra.includes(INPUT2[i])) {
             console.log(INPUT2[i], 'amarillo')
-            SPAN.style.backgroundColor = 'yellow';
+            SPAN.style.backgroundColor = '#f3c237';
+
             SPAN.innerHTML = INPUT2[i]
         } else {
             console.log(INPUT2[i], 'gris')
-            SPAN.style.backgroundColor = 'gray';
+            SPAN.style.backgroundColor = '#a4aec4';
+
             SPAN.innerHTML = INPUT2[i]
         }
         ROW.appendChild(SPAN)
     }
     GRID.appendChild(ROW)
-    intento = intento - 1;
+    intento--
     if (intento == 0) {
-    console.log('perdiste')
+    console.log('PERDISTE!')
+    terminar("<h1>PERDISTE!</h1>")
     return;
 }}
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
